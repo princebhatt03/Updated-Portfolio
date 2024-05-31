bd7f6ad57733976559b2d2b5592fb1826f8420fc
Prince Bhatt <princebhatt316@gmail.com>
2024-05-31 16:38:39 +0530

    Website Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -307,3 +307,17 @@ form.addEventListener('submit', e => {
 
   sendEmail();
 });
+
+window.addEventListener('load', function () {
+  var preloader = document.getElementById('preloader-active');
+  setTimeout(function () {
+    preloader.style.transition = 'opacity 0.6s';
+    preloader.style.opacity = '0';
+    preloader.addEventListener('transitionend', function () {
+      preloader.style.display = 'none';
+    });
+  }, 450);
+  setTimeout(function () {
+    document.body.style.overflow = 'visible';
+  }, 450);
+});
